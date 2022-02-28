bc25f358c31be9177f4e195b0bac2819024452c3
Roeck <rons9n@gmail.com>
2022-02-28 01:03:15 -0500

    TransactionContext

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { TransactionsProvider } from './context/TransactionContext'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <TransactionsProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </TransactionsProvider>,
+      document.getElementById('root')
 )
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './Transactions.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = '0xD62931a6b630C95D69f7e303F5B7732980FefB62'
+export const contractAddress= '0x12fb7468762FE73246c797225720739ddBdF0Fd7'
